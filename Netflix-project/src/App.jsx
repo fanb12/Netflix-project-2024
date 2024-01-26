233430aca364db9735e7f19cb630efc91846d395
fanb12 <fantaneshbezabh2@gmail.com>
2024-01-27 01:33:13 +0300

    header and footer

--- Netflix-project/src/App.jsx
+++ Netflix-project/src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import Home from "./page/home/Home";
+import Header from "./component/Header/Header";
+import Footer from "./component/Footer/Footer";
 
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <Header />
+      <Footer />
     </>
   );
 }
