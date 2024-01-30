43fc05d2dc0e8b94f5eabc9bcd84f043bf805c91
fanb12 <fantaneshbezabh2@gmail.com>
2024-01-30 03:42:34 +0300

    updated

--- Netflix-project/src/utils/requests.jsx
+++ Netflix-project/src/utils/requests.jsx
@@ -1,16 +1,16 @@
-// const API_KEY = process.env.REACT_APP_API_KEY;
+//const API_KEY = process.env.REACT_APP_API_KEY;
 const API_KEY = "fd00f41e7b31fb1c93a86c241c6eb510";
 const requests = {
   fetchTrending: `/tv/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOrginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRateMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&genre_ids=35`,
-  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&genre_ids=35`,
-  fetchAnimationMovies: `/discover/movie?api_key=${API_KEY}&genre_ids=16`,
-  fetchRomanticMovies: `/discover/movie?api_key=${API_KEY}&genre_ids=10749`,
-  fetchDocumenraries: `/discover/movie?api_key=${API_KEY}&genre_ids=99`,
-  fetchMusicMovies: `/discover/movie?api_key=${API_KEY}&genre_ids=10402`,
-  fetchAdvintureMovies: `/discover/movie?api_key=${API_KEY}&genre_ids=12`,
-  fetchTvShows: `/tv/popular?api_key=${API_KEY}language=en-US&page=1`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchAnimationMovies: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+  fetchRomanticMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumenraries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchMusicMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10402`,
+  fetchAdvintureMovies: `/discover/movie?api_key=${API_KEY}&with_genres=12`,
+  fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
 };
 export default requests;
