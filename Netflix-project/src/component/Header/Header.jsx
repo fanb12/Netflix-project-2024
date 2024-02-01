0d6a4d5fb52f92fff956a9f259f465880876527c
fanb12 <fantaneshbezabh2@gmail.com>
2024-02-01 22:19:11 +0300

    deploy

--- Netflix-project/src/component/Header/Header.jsx
+++ Netflix-project/src/component/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 import logo from "../../image/-11621687998ytrgnduxoh.png";
 import SearchIcon from "@mui/icons-material/Search";
@@ -6,16 +6,26 @@ import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import { Link } from "react-router-dom";
+//import "bootstrap/dist/css/bootstrap.css";
 
 function Header() {
+  const [isListOpen, setIsListOpen] = useState(false);
+
+  const toggleList = () => {
+    setIsListOpen(!isListOpen);
+  };
   return (
     <div className="header_outer_container">
       <div className="header_container">
+        <div className="hi">
+          <img src={logo} alt="Netflix" width="100" />
+        </div>
         <div className="header_left">
-          <ul>
-            <li>
-              <img src={logo} alt="Netflix" width="100" />
-            </li>
+          <button id="toggle-button" onClick={toggleList}>
+            ☰
+          </button>
+
+          <ul id="toggle-list" className={isListOpen ? "show" : ""}>
             <li>
               <Link to="/">Home</Link>
             </li>
